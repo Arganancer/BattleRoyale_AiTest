bb1999ca4621a20013b6547dbf294275a0bc2137
1664356 <1664356@etu.cegep-ste-foy.qc.ca>
2018-08-23 16:27:09 -0400

    Added normal behaviour file

---
+++ Assets/Scripts/Playmode/Ennemy/Strategies/NormalBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Playmode.Ennemy.Strategies;
+using UnityEngine;
+
+namespace Playmode.Ennemy.Strategies
+{
+    public class NormalBehaviour : IEnnemyStrategy {
+        public void Act()
+        {
+            
+        }
+    }
+}
+
