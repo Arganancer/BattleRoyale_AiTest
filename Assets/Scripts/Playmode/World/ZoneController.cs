48380e2d89a25707b13a7eeca0d0f122dad81aec
1664356 <1664356@etu.cegep-ste-foy.qc.ca>
2018-08-30 14:20:39 -0400

    Working on camera

--- Assets/Scripts/Playmode/World/ZoneController.cs
+++ Assets/Scripts/Playmode/World/ZoneController.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class ZoneController : MonoBehaviour
 {
-	[SerializeField] float timeBufferToMoveZone = 10f;
-	[SerializeField] private int startingRadiusZoneSize = 90;
+	[SerializeField] float timeBufferToMoveZone = 120f;
+	[SerializeField] private int startingRadiusZoneSize = 10;
 	private float timeOfLastShrink = 0;
 	private CircleCollider2D zoneCollider2D;
-	[SerializeField] private int minimunSizeShrink = 10;
-	[SerializeField] private int maximumSizeShrink = 30;
-	private const float SPRITESCALESIZE = 0.25f;
-	private const int MAXSHRINKINGSIZE = 10;
+	[SerializeField] private int minimunSizeShrink = 2;
+	[SerializeField] private int maximumSizeShrink = 3;
+	private const float SPRITESCALESIZE = 0.0625f;
+	private const int MAXSHRINKINGSIZE = 2;
 	private float nextRadius = 0;
-	[SerializeField] private int sizeReduction = 2;
+	[SerializeField] private float sizeReduction = 0.5f;
 	private int shrinkSpeedBuffer = 0;
-	private const int MAXSHRINKSPEEDBUFFER = 16;
+	private const int MAXSHRINKSPEEDBUFFER = 100;
 	private GameObject zoneRenderer;
 	private void Awake()
 	{
@@ -24,12 +24,13 @@ public class ZoneController : MonoBehaviour
 		zoneRenderer = GameObject.Find("ZoneRenderer");
 		zoneCollider2D.radius = startingRadiusZoneSize;
 		nextRadius = startingRadiusZoneSize;
-		zoneRenderer.transform.localScale = new Vector3(11.5f,11.5f,0);
+		zoneRenderer.transform.localScale = new Vector3(1.26f,1.26f,0);
 	}
 
 	private void Update()
 	{
 		ChangeZonePositionAndSize();
+		zoneRenderer.transform.Rotate(Vector3.back);
 	}
 
 	private void ChangeZonePositionAndSize()
@@ -43,7 +44,8 @@ public class ZoneController : MonoBehaviour
 
 			timeOfLastShrink = Time.time;
 		}
-		else if (GetZoneRadius() > nextRadius)
+		else if (GetZoneRadius() > nextRadius && zoneRenderer.transform.localScale.x > SPRITESCALESIZE
+		         && GetZoneRadius() - sizeReduction >1)
 		{
 			ShrinkZone();
 		}
@@ -51,15 +53,18 @@ public class ZoneController : MonoBehaviour
 
 	void ShrinkZone()
 	{
-		if (shrinkSpeedBuffer > MAXSHRINKSPEEDBUFFER)
-		{
-			zoneCollider2D.radius -= sizeReduction;
-			ChangeSpriteScale();
-			ChangeSpritePosition();
-			shrinkSpeedBuffer = 0;
-		}
-
-		shrinkSpeedBuffer++;
+		zoneCollider2D.radius -= sizeReduction/MAXSHRINKSPEEDBUFFER;
+		ChangeSpriteScale();
+		ChangeSpritePosition();
+//		if (shrinkSpeedBuffer > MAXSHRINKSPEEDBUFFER)
+//		{
+//			zoneCollider2D.radius -= sizeReduction/MAXSHRINKSPEEDBUFFER;
+//			ChangeSpriteScale();
+//			ChangeSpritePosition();
+//			shrinkSpeedBuffer = 0;
+//		}
+//
+//		shrinkSpeedBuffer++;
 	}
 	void ChangeZoneColliderOffset()
 	{
@@ -96,8 +101,8 @@ public class ZoneController : MonoBehaviour
 	void ChangeSpriteScale()
 	{
 		zoneRenderer.transform.localScale = 
-			new Vector3(GetCurrentZoneSpriteScale() - SPRITESCALESIZE,
-			GetCurrentZoneSpriteScale() - SPRITESCALESIZE,0);
+			new Vector3(GetCurrentZoneSpriteScale() - SPRITESCALESIZE/MAXSHRINKSPEEDBUFFER,
+			GetCurrentZoneSpriteScale() - SPRITESCALESIZE/MAXSHRINKSPEEDBUFFER,0);
 	}
 
 	void ChangeSpritePosition()
