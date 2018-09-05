1ece7011384a1670abb28bb0622e2bd96d6551be
Kasandra Brassard <1463457@etu.cegep-ste-foy.qc.ca>
2018-09-05 15:47:25 -0400

    clean + refactor

---
+++ Assets/Scripts/Playmode/Sound/SoundEnablingController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Playmode.Sound
+{
+	public class SoundEnablingController : MonoBehaviour
+	{
+		public void PlayAndStopAudio()
+		{
+			AudioListener.pause = !AudioListener.pause;
+		}
+	}
+}
